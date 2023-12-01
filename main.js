af2e8dca8c94fc25e38f4b6a5a432c12e8337cec
ibrahim <himaovic1@gamil.com>
2023-12-01 08:20:03 +0200

    update the store

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-let btn = document.getElementById('click');
-window.onscroll = function(){     
-    if(scrollY>=300){
-       btn.style.display = 'block'
-    }
-    else{
-        btn.style.display = 'none'
-    }
-}
-btn .onclick = function(){
-    scroll({
-        left: 0,
-        top: 0,
-        behavior:"smooth"
-    })
-}
+// let btn = document.getElementById('click');
+// window.onscroll = function(){     
+//     if(scrollY>=300){
+//        btn.style.display = 'block'
+//     }
+//     else{
+//         btn.style.display = 'none'
+//     }
+// }
+// btn .onclick = function(){
+//     scroll({
+//         left: 0,
+//         top: 0,
+//         behavior:"smooth"
+//     })
+// }
 
 
 
-let img = document.getElementById('main')
-let small1 = document.getElementById('small1')
-let small2 = document.getElementById('small2')
-let small3 = document.getElementById('small3')
-let small4 = document.getElementById('small4')
+// let img = document.getElementById('main')
+// let small1 = document.getElementById('small1')
+// let small2 = document.getElementById('small2')
+// let small3 = document.getElementById('small3')
+// let small4 = document.getElementById('small4')
 
-function hello(pic){
-    img.src = pic
-}
+// function hello(pic){
+//     img.src = pic
+// }
