184076845f54f41bdbe456fb825fa1b279dcef47
ibrahim <himaovic1@gamil.com>
2023-12-03 12:39:59 +0200

    update the price

--- main.js
+++ main.js
@@ -17,12 +17,13 @@
 
 
 
-// let img = document.getElementById('main')
-// let small1 = document.getElementById('small1')
-// let small2 = document.getElementById('small2')
-// let small3 = document.getElementById('small3')
-// let small4 = document.getElementById('small4')
+ let img = document.getElementById('main')
+ let small1 = document.getElementById('small1')
+ let small2 = document.getElementById('small2')
+ let small3 = document.getElementById('small3')
+ let small4 = document.getElementById('small4')
+console.log(small1)
 
-// function hello(pic){
-//     img.src = pic
-// }
+function hello(pic){
+    img.src = pic
+}
